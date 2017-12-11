ff50c2b64546e211e2c5bd9e380dc92e31bbb77d
Duy Phuoc <duyphuoc@duyphuoc.DPOS>
2017-12-11 16:11:45 +0700

    missing ')'

--- src/schema/types/api.js
+++ src/schema/types/api.js
@@ -18,4 +18,5 @@ export default new GraphQLObjectType({
     childs: {
       type: new GraphQLList(Api)
     }
+  })
 });
--- src/schema/types/post.js
+++ src/schema/types/post.js
@@ -17,6 +17,5 @@ export default new GraphQLObjectType({
     content: {
       type: GraphQLString
     }
-
   })
 });
